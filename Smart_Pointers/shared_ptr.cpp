f804a6149284dabaeae337dd141dbacab8c16d2b
aramusayelyan <musayelyanara6@gmail.com>
2024-09-22 21:16:51 +0400

    Start

---
+++ Smart_Pointers/shared_ptr.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+
+template<typename T>
+class shared_ptr{
+    private:
+        T* ptr;
+        int * control;
+    public:
+        shared_ptr(T * _ptr){
+            ptr = _ptr;
+            control = new int;
+            *control = 1;
+        }
+        ~shared_ptr(){
+            if(!((*control)--)){
+                delete ptr;
+                delete control;
+            }
+        }
+        shared_ptr(const shared_ptr& other){
+            this->ptr = other.ptr;
+            this->ptr = other.control; 
+        }
+        shared_ptr(shared_ptr&& other){
+            this->ptr = other.ptr;
+            other.ptr = nullptr;
+            this->control = other.control;
+            other.control = nullptr;
+        }
+        void  swap(shared_ptr& other){
+            std::swap(this->ptr, other.ptr);
+            std::swap(this->control, other.control);
+        }
+        unique_ptr& operator=(shared_ptr&& other){
+            unique_ptr<T> tmp(other);
+            swap(*this,tmp);
+            return *this; 
+        }
+        unique_ptr& operator=(const shared_ptr& other){
+            if(ptr && control){
+                delete ptr;
+                delete control;
+            }
+            unique_ptr<T> tmp(other);
+            swap(*this,tmp);
+            return *this;
+        }
+        T* get(){
+            return ptr;
+        }
+T* release(){
+    T* tmp = ptr;
+    delete ptr;
+    return tmp;
+}
+
+        
+};
+
+int main(){
+
+}
---
+++ Smart_Pointers/unique_ptr.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+
+template <typename T>
+class unique_ptr{
+    private:
+        T* ptr;
+    public:
+        unique_ptr(T* _ptr){
+            ptr = _ptr;
+        }
+        ~unique_ptr(){
+            delete ptr;
+        }
+        unique_ptr(const unique_ptr & other) = delete;
+
+        unique_ptr(unique_ptr && other) {
+            this->ptr = other.ptr;
+            other.ptr = nullptr;
+        }
+
+        unique_ptr& operator=(unique_ptr && other){ 
+            unique_ptr<T> tmp(other);
+            swap(tmp);
+            return *this;
+        }
+
+        void swap(unique_ptr& b){
+            std::swap(this->ptr,b.ptr);
+        }
+
+        T* release(){
+            T* tmp = ptr;
+            delete ptr;
+            return tmp;
+        }
+
+        void reset(T* _new){
+            delete ptr;
+            ptr = _new;
+        }
+
+        T* get(){
+            return ptr;
+        }
+
+        T& operator*() const{
+            return *ptr;
+        }
+
+        T* operator->() const {
+            return ptr;
+        }
+
+};
+
+int main(){
+    unique_ptr<int> a(new int);
+    *a = 5;
+    unique_ptr<int> b = std::move(a);
+
+    std::cout << *b;
+}
